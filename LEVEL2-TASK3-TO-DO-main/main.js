48decb22d1f89c64d1c6acc9a1596b0fb8663dbd
sudarshanmakar001 <63312264+sudarshanmakar001@users.noreply.github.com>
2022-08-29 20:44:22 +0530

    Add files via upload

---
+++ LEVEL2-TASK3-TO-DO-main/main.js
@@ -0,0 +1,64 @@
+const addButton = document.querySelector('.addButton');
+var input = document.querySelector('.input');
+const container = document.querySelector('.container');
+
+
+class item {
+    constructor(itemName) {
+
+
+        this.createDiv(itemName);
+
+    }
+
+    createDiv(itemName) {
+
+        let input = document.createElement('input');
+        input.value = itemName;
+        input.disabled = true;
+        input.classList.add('item_input')
+        input.type = "text";
+
+        let itemBox = document.createElement('div');
+        itemBox.classList.add('item')
+
+        let editButton = document.createElement('button');
+
+
+        let removeButton = document.createElement('button');
+        removeButton.innerHTML = "REMOVE";
+        removeButton.classList.add('removeButton');
+
+        container.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(editButton);
+        itemBox.appendChild(removeButton);
+
+
+
+        removeButton.addEventListener('click', () => this.remove(itemBox));
+
+    }
+    remove(item) {
+
+        container.removeChild(item);
+    }
+
+}
+
+function check() {
+
+    if (input.value != "") {
+        new item(input.value);
+        input.value = "";
+    }
+
+}
+
+addButton.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+    if (e.which == 13) {
+        check();
+    }
+});
